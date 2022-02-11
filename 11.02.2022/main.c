b2847216c4afe267298761ce6c291ddaa3c71f1e
Nikita <falkorickman@gmail.com>
2022-02-11 14:44:18 +0100

    Home work

---
+++ 11.02.2022/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    //Create
+    int     Int =   10;
+    float   Float=  17.42454F;
+    char    Char[]= "scutum";
+    double  Double= 22.5;
+
+    //output
+    printf("%d\n",Int);
+    printf("%f\n",Float);
+    printf("%s\n",Char);
+    printf("%lf\n",Double);
+    return 0;
+}
